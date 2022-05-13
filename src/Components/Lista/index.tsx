667991ee9cbc87929a10e194a648526cd310e7aa
AlisonCavalcante <alison.rscavalcante@gmail.com>
2022-05-12 22:55:52 -0300

    add useState Lista

--- src/Components/Lista/index.tsx
+++ src/Components/Lista/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./item";
-import styles from './Lista.module.scss';
+import styles from "./Lista.module.scss";
 
 function Lista() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {
       tasks: "React",
       time: "02:00:00",
@@ -12,20 +12,26 @@ function Lista() {
       tasks: "Angular",
       time: "01:00:00",
     },
-  ];
+  ]);
 
   return (
     <aside className={styles.listaTarefas}>
-      <h2>Estudos do dia</h2>
+      <h2
+        onClick={() => {
+          setTasks([...tasks, { tasks: "Redux", time: "05:00:00" }]);
+        }}
+      >
+        Estudos do dia
+      </h2>
       <ul>
-          {tasks.map((item, index) => (
-              <Item
-              // tasks={item.tasks} Um forma de passar o props
-              // time={item.time}
-              key={index}
-              {...item} // Segunda Forma, desestruturando o objeto item e pegando todas as Keys dele
-              />
-          ))}
+        {tasks.map((item, index) => (
+          <Item
+            // tasks={item.tasks} Um forma de passar o props
+            // time={item.time}
+            key={index}
+            {...item} // Segunda Forma, desestruturando o objeto item e pegando todas as Keys dele
+          />
+        ))}
       </ul>
     </aside>
   );
